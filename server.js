da9c03270d3b19123ca159592f4defbba63658b6
Frieda Abdo <friedabdo@gmail.com>
2020-10-20 21:30:58 -0400

    que

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
+require("dotenv").config();
+const { PORT = 4500, NODE_ENV = "development" } = process.env;
+
+const mongoose = require("./db/connection")
+
+const cors = require("cors");
+const corsOptions = require("./configs/cors.js")
+
 const express = require('express')
 const app = express()
 
 // Add the middleware code needed to accept incoming data and add it to req.body
-
+NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors());
+app.use(express.json());
 const logger = require('morgan')
 app.use(logger('dev'))
 app.use(express.urlencoded({extended:false}))
-app.use(express.json())
+
+
+
 
 app.get('/', (req, res) => {
     res.json({
@@ -22,4 +33,4 @@ app.use('/api/cookbooks/', cookbookRouter)
 const authorRouter = require('./controllers/authorRoutes')
 app.use('/api/authors/', authorRouter)
 
-app.listen(4000, () => console.log('Server running on port 4000!'))
+app.listen(PORT, () => console.log('Server running on port 4500!'))
